4c88cfec557755a3fd5e7ca737c68f75359c7388
Carlos Mantilla <cmantilla@aliongroo.com>
2014-11-25 10:16:40 +0200

    Refactor generator to prototype implementation.

--- app/index.js
+++ app/index.js
@@ -9,46 +9,55 @@ var AngularElementGenerator = yeoman.generators.Base.extend({
     this.pkg = require('../package.json');
   },
 
-  prompting: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the Angular Element generator!'
-    ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
-      done();
-    }.bind(this));
-  },
+  prompting: this.askFor,
 
   writing: {
-    app: function () {
-      this.dest.mkdir('app');
-      this.dest.mkdir('app/templates');
-
-      this.src.copy('_package.json', 'package.json');
-      this.src.copy('_bower.json', 'bower.json');
-    },
-
-    projectfiles: function () {
-      this.src.copy('editorconfig', '.editorconfig');
-      this.src.copy('jshintrc', '.jshintrc');
-    }
+    app: this.writeApp,
+    projectFiles: this.writeProjectFiles
   },
 
-  end: function () {
-    this.installDependencies();
+  end: {
+    install: this.install
   }
 });
+
+AngularElementGenerator.prototype.askFor = function() {
+  var done = this.async();
+
+  // Have Yeoman greet the user.
+  this.log(yosay(
+    'Welcome to the Angular Element generator!'
+  ));
+
+  var prompts = [{
+    type: 'confirm',
+    name: 'installAngular',
+    message: 'Would you like to install angular application?',
+    default: true
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.installAngular = props.installAngular;
+
+    done();
+  }.bind(this));
+};
+
+AngularElementGenerator.prototype.writeApp = function() {
+  this.dest.mkdir('app');
+  this.dest.mkdir('app/templates');
+
+  this.src.copy('_package.json', 'package.json');
+  this.src.copy('_bower.json', 'bower.json');
+};
+
+AngularElementGenerator.prototype.writeProjectFiles = function() {
+  this.src.copy('editorconfig', '.editorconfig');
+  this.src.copy('jshintrc', '.jshintrc');
+};
+
+AngularElementGenerator.prototype.install = function() {
+  this.installDependencies();
+};
 
 module.exports = AngularElementGenerator;
